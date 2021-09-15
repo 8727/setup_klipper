a9920bf30f5052568804254866a8fbc4b537d246
Alex <server.8727@yandex.ru>
2021-09-15 17:17:10 +0300

    upd

--- backup_email.sh
+++ backup_email.sh
@@ -23,4 +23,3 @@ msg='Klipper : '$Klipper' \nMoonraker : '$Moonraker' \nMainsail : '$Mainsail
 # Отправляем письмо
 /usr/bin/sendEmail -f $FROM -t $MAILTO -o message-charset=utf-8 -a /home/$USER/BackUp.zip -u $tema -m $msg -s $SMTPSERVER -o tls=yes -xu $SMTPLOGIN -xp $SMTPPASS
 rm /home/$USER/BackUp.zip		# Удаляем файл backup
-# crontab -e		0 05 * * * /home/$USER/backup_email.sh

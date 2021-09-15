3ef2e8ff0300be92ad85b0fa2a9fe77814740b9d
Alex <n003.neo@yandex.ru>
2021-09-15 22:37:50 +0300

    upd

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# This script helps to install klipper on orangepi devices
 
 clear
-set -e
-
-apt update -y && apt upgrade -y
-apt-get install gpiod sendemail libnet-ssleay-perl libio-socket-ssl-perl -y
+sudo apt update -y && sudo apt upgrade -y
+sudo apt-get install gpiod sendemail libnet-ssleay-perl libio-socket-ssl-perl -y
 
 NEWUSER='klipper'
 
 create_klipper_user(){
-  if [[ $(cat /etc/passwd | grep 'klipper' | wc -l) -eq 0 ]]; then
-      adduser ${NEWUSER}
-  fi
+	if [[ $(cat /etc/passwd | grep 'klipper' | wc -l) -eq 0 ]]; then
+	    adduser ${NEWUSER}
+	fi
     usermod -a -G tty ${NEWUSER}
     usermod -a -G dialout ${NEWUSER}
     adduser ${NEWUSER} sudo
@@ -25,15 +24,15 @@ update_udev(){
 #
 SUBSYSTEM=="gpio", GROUP="gpio", MODE="0660"
 SUBSYSTEM=="gpio*", PROGRAM="/bin/sh -c '\
- chown -R root:gpio /sys/class/gpio && chmod -R 770 /sys/class/gpio;\
- chown -R root:gpio /sys/devices/virtual/gpio && chmod -R 770 /sys/devices/virtual/gpio;\
- chown -R root:gpio /sys$devpath && chmod -R 770 /sys$devpath\'"
+        chown -R root:gpio /sys/class/gpio && chmod -R 770 /sys/class/gpio;\
+        chown -R root:gpio /sys/devices/virtual/gpio && chmod -R 770 /sys/devices/virtual/gpio;\
+        chown -R root:gpio /sys$devpath && chmod -R 770 /sys$devpath\'"
 
 EOF
 
     # execute udev rules?!
     if [[ `cat /etc/group | grep 'gpio' | wc -l` -eq 0 ]]; then
-      groupadd gpio
+    	groupadd gpio
     fi
     usermod -a -G gpio ${NEWUSER}
     udevadm control --reload-rules
@@ -42,7 +41,6 @@ EOF
 
 create_klipper_user
 update_udev
-echo -e '0 05 * * * /home/klipper/setup_klipper/backup_email.sh' | crontab -
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 sudo -Hu ${NEWUSER} ${SCRIPT_DIR}/services.sh

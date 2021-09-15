84cd7cc538706fa7ab7cad5e3b8a787ed3b3f8de
Alex <server.8727@yandex.ru>
2021-09-15 17:12:17 +0300

    update email

--- backup_email.sh
+++ backup_email.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 USER='klipper'
-
 versions(){
   cd /home/$USER/klipper/
   Klipper=$(git describe HEAD --always --tags | cut -d "-" -f 1,2)
@@ -10,36 +9,18 @@ versions(){
   Mainsail=$(head -n 1 /home/$USER/mainsail/.version)
   cd ~
 }
-
 data=`date +%Y.%m.%d`
 tema='3D Printer Klipper Backup Configs '$data
-
+zip -r /home/$USER/BackUp.zip klipper_config/* setup_klipper/*	# Делаем бэкап папки
 versions
-echo -e "$Klipper"
-echo -e "$Moonraker"
-echo -e "$Mainsail"
-# Делаем бэкап папки
-zip -r /home/$USER/BackUp.zip klipper_config/* setup_klipper/*
-# Будет отображаться "От кого"
-FROM=sis.8727@gmail.com
-
-# Кому
-MAILTO=5796630@mail.ru
-
-# В моем примере я отправляю письма через существующий почтовый ящик на gmail.com
-# Скрипт легко адаптируется для любых почтовых серверов
+echo -e 'Klipper : '$Klipper' \tMoonraker : '$Moonraker' \tMainsail : '$Mainsail
+FROM=sis.8727@gmail.com			# Будет отображаться "От кого"
+MAILTO=5796630@mail.ru			# Кому
 SMTPSERVER=smtp.gmail.com:587
-
-# Логин и пароль от учетной записи gmail.com
-SMTPLOGIN=sis.8727@gmail.com
+SMTPLOGIN=sis.8727@gmail.com	# Логин и пароль от учетной записи gmail.com
 SMTPPASS=c2h5ohh2oso4
-
-# Сообщение
 msg='Klipper : '$Klipper' \nMoonraker : '$Moonraker' \nMainsail : '$Mainsail
-
 # Отправляем письмо
 /usr/bin/sendEmail -f $FROM -t $MAILTO -o message-charset=utf-8 -a /home/$USER/BackUp.zip -u $tema -m $msg -s $SMTPSERVER -o tls=yes -xu $SMTPLOGIN -xp $SMTPPASS
-
-# Удаляем файл backup
-rm /home/$USER/BackUp.zip
+rm /home/$USER/BackUp.zip		# Удаляем файл backup
 # crontab -e		0 05 * * * /home/$USER/backup_email.sh
